8ec3aec1347112fb9d561a684fa83d39322f57a0
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-10-28 19:51:23 +0530

    [VIGNESH M] ADDED: InsertAt and InsertBetween method to the LinkedList

--- LinkedList.cs
+++ LinkedList.cs
@@ -76,5 +76,45 @@ namespace LinkedListDataStructure
             }
             Console.WriteLine("{0} appended into linked list", node.data);
         }
+
+        internal void InsertAt(int position,int data)
+        {
+            Node node = new Node(data);
+            if(position < 1)
+            {
+                Console.WriteLine("Invalid position");
+            }
+            else
+            {
+                Node temp = head;
+                while(position != 2)
+                {
+                    temp = temp.next;
+                    position--;
+                }
+                node.next = temp.next;
+                temp.next = node;
+            }
+        }
+
+        internal void InsertBetween(int new_data)
+        {
+            Node new_node = new Node(new_data);
+            Node pos = head;
+            int len = 0;
+            while(pos != null)
+            {
+                len++;
+                pos = pos.next;
+            }
+            int count = ((len % 2 == 0) ? (len / 2) : (len + 1)) / 2;
+            pos = head;
+            while(count-- > 1)
+            {
+                pos = pos.next;
+            }
+            new_node.next = pos.next;
+            pos.next = new_node;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,8 +9,11 @@ namespace LinkedListDataStructure
             Console.WriteLine("Welcome To Linked List Data Structure");
             LinkedList list = new LinkedList();            
             list.AppendList(56);
-            list.AppendList(30);
             list.AppendList(70);
+            list.Display();
+            Console.WriteLine();
+            list.InsertAt(2, 30);
+            Console.WriteLine("30 Is Inserted Between 56 and 70");
             list.Display();
             Console.ReadLine();
         }
