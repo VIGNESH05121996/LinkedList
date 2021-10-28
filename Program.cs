e92057a3be8171334b82d0c1b28a658179d05a08
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-10-28 18:36:50 +0530

    [VIGNESH M] ADDED: Welcome Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedListDataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Linked List Data Structure");
+        }
+    }
+}
