52d2f044c3fa94bbc697b2316b27967d50affb2e
VIGNESH M <vickymarimuthu0512@gmail.com>
2021-10-28 19:03:16 +0530

    [VIGNESH M] ADDED: Simple Linked List Data Structure

--- Program.cs
+++ Program.cs
@@ -7,6 +7,12 @@ namespace LinkedListDataStructure
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Linked List Data Structure");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            Console.ReadLine();
         }
     }
 }
